60f6a212fbb52b4ce44b92b48961937443faf442
Anonymous Coward <nobody@lol.com>
2015-07-09 14:04:52 -0400

    Config variables are now written to projectname.env in current directory. - renamed project from ss to shopsync to avoid name conflict with iproute2 on linux. - errors on nil/empty input.
    Signed-off-by: Mike Enriquez <mike@enriquez.me>

--- lib/shop_sync/cli.rb
+++ lib/shop_sync/cli.rb
@@ -1,11 +1,30 @@
 require 'thor'
+require 'fileutils'
 
 module ShopSync
   class CLI < Thor
 
     desc "config STORE", "Generate a configuration file for the Shopify store named STORE"
     def config store
+      api_key = ask("What is your API key?\n", :echo => false)
+      api_password = ask("What is your API password?\n", :echo => false)
+      api_host = ask("What is your API host?\n")
 
+      api_options = Array.new
+      [api_key, api_password, api_host].each do |config_option|
+        no_whitespace = config_option.strip
+        if no_whitespace.empty?
+          say("Invalid option. Please try again.")
+          return
+        end
+        api_options.push(no_whitespace)
+      end
+
+      config_file = File.new("#{store}"+".env", "w")
+      config_file.write("API_KEY=#{api_options[0]}\n")
+      config_file.write("API_PASSWORD=#{api_options[1]}\n")
+      config_file.write("API_HOST=#{api_options[2]}\n")
+      say("Your config file is available at #{config_file.path}")
     end
 
     desc "download STORE", "Download the theme assets into the current directory for the configured STORE"
